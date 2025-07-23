1557c5eb7e7954f3d0cdfcbdc04dc19d851b54d6
Vakkas Emre Yıldız <72315833+vakkas1234@users.noreply.github.com>
2025-07-23 21:23:20 +0300

    Update client.js

--- client.js
+++ client.js
@@ -2,14 +2,16 @@ const io = require('socket.io-client');
 const screenshot = require('screenshot-desktop');
 const { exec } = require('child_process');
 
-const socket = io('http://192.168.1.153:3000'); // Sunucu adresi
+// Bağlanmak istediğiniz sunucu adresini buraya yazın (ör: http://sunucu_ip:3000 veya http://domain.com:3000)
+const SERVER_URL = '192.168.1.176:3000';
+const socket = io(SERVER_URL); // Sunucu adresi
 
 socket.on('connect', () => {
     console.log('Sunucuya bağlandı.');
 });
 
 socket.on('komut', async (komut) => {
-    console.log(`Gelen komut: ${komut}`);
+    console.log(`Gelen komut:`, komut);
 
     if (komut === 'screenshot') {
         try {
@@ -23,7 +25,20 @@ socket.on('komut', async (komut) => {
 
     if (komut === 'shutdown') {
         console.log("Sistem kapatılıyor (gerçekten değil, simülasyon)");
-         exec('shutdown -s -t 0');
+        exec('shutdown -s -t 0');
+    }
+
+    if (typeof komut === 'object' && komut.type === 'nircmd') {
+        // komut.args: kullanıcıdan gelen parametreler
+        exec(`nircmd.exe ${komut.args}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`nircmd hatası: ${error}`);
+            } else {
+                console.log('nircmd komutu çalıştırıldı:', komut.args);
+            }
+        });
     }
 });
+
+
 
